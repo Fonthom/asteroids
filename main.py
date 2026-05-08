d010588bf59730eb398d9739e0e5e89fc1051c79
Fonthom <thomasf0n7a1n3@gmail.com>
2026-05-08 20:58:17 +0200

    A: first step to asteroids

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    While True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
